f67d097b527fba22bb985bcfe2e9a15dacdb9757
Oleksandr Filonenko <filonenko.oleksa@gmail.com>
2024-01-28 13:10:21 +0200

    Add the algorithm for the checking if number is even

---
+++ src/main/java/edu/xfoleks/algorithms/bitmanipulation/EvenNumberChecker.java
@@ -0,0 +1,22 @@
+package edu.xfoleks.algorithms.bitmanipulation;
+
+import java.util.Scanner;
+
+public class EvenNumberChecker {
+    public static boolean isEvenNumber(int number) {
+        return (number & 1) == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter any integer number: ");
+
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        if (!isEvenNumber(number)) {
+            System.out.println("The given number isn't the even");
+        } else {
+            System.out.println("The given number is the even");
+        }
+    }
+}
