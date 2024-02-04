085035a0409e8b69727bdfa1fb5f1ee44306258d
Oleksandr Filonenko <filonenko.oleksa@gmail.com>
2024-02-04 20:13:34 +0200

    Add BufferedReader algorithm

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 This repository is containing the next groups of algorithms:
 
 - Bit manipulation
+- Input/output (buffered reader)
 
 
 Java 17 - the version of the programing language is used for creation of this 
---
+++ src/main/java/edu/xfoleks/algorithm/io/BufferedReaderApp.java
@@ -0,0 +1,29 @@
+package edu.xfoleks.algorithm.io;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class BufferedReaderApp {
+
+    public static void main(String[] args) throws IOException {
+        String text = "Hello World!";
+        int lengthOfString = text.length();
+        byte[] bytes = text.getBytes(StandardCharsets.UTF_8);
+
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
+        BufferedReader bufferedReader = new BufferedReader(inputStream);
+
+        System.out.printf("First symbol is - %s\n", bufferedReader.read());
+        lengthOfString--;
+        System.out.printf("Count of the available bytes is: %d and length of string is %d\n",
+                bufferedReader.getNumberOfAvailableBytes(), lengthOfString);
+        System.out.printf("Second symbol is - %s\n", bufferedReader.peek(1));
+        System.out.printf("Fourth symbol is - %s\n", bufferedReader.peek(3));
+
+
+
+        System.out.printf("The block: %s", Arrays.toString(bufferedReader.readBlock()));
+    }
+}
